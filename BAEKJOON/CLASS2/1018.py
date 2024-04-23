6e153706c4a64dd915ce46efefb4028947ebb663
2018007956 <bbmt1906@hanyang.ac.kr>
2024-04-23 18:38:08 +0900

    2024-04-23

--- BAEKJOON/CLASS2/1018.py
+++ BAEKJOON/CLASS2/1018.py
@@ -1,8 +1,56 @@
+# 2024-04-23
+def count_diff(arr):
+    # 1: 왼쪽 위가 W인 경우
+    ans = ['WBWBWBWB', 'BWBWBWBW']
+    cnt_w = 0
+    k=0
+    for i in range(8):
+        for j in range(8):
+            if arr[i][j]!=ans[k][j]:
+                cnt_w += 1
+        k+=1
+        k%=2
+
+    # 2: 왼쪽 위가 B인 경우
+    cnt_b = 0
+    k=1
+    for i in range(8):
+        for j in range(8):
+            if arr[i][j]!=ans[k][j]:
+                cnt_b += 1
+        k+=1
+        k%=2
+    return min(cnt_w, cnt_b)
+
+# Input
+N, M = map(int, input().split())
+
+board = []
+for i in range(N):
+    board.append(input())
+
+# Bruth force
+result = []
+for i in range(N-7):
+    for j in range(M-7):
+        result.append(count_diff([row[j:j+8] for row in board[i:i+8]]))
+print(min(result))
+
+'''
+5:10~6:00 (50m) 생각한대로 다 구현했는데 Index Error 뜨는 상황
+4:42~11:02 (20m)
+    문제 : 2차원 배열 slicing이 제대로 안됨
+    해결 : board[i:i+8][j:j+8] -> [row[j:j+8] for row in board[i:i+8]]
+
+작년에 풀어서 틀렸던 아래 코드도 같은 문제였음. 해당 부분 수정하니 성공
+'''
+
+# 2023-01-06
 N, M = map(int, input().split())
 
 def start_(i, s):
     cnt = []
-    if i==0 or i%2 == 0: # 홀수번째 row
+    if i%2 == 0:
         case = str(s*M)[:M]
     else:
         case = str(s[::-1]*M)[:M]
@@ -24,21 +72,19 @@ for i in range(N):
     start_b.append(start_(i, 'BW'))
     
 # 8X8
-print(len(start_w), len(start_w[0]))
 result = []
-for i in range(N-8+1): # 0~2
-    for j in range(M-8+1): # 0~16
-        w = sum(sum(start_w[i:i+8][j:j+8], []))
-        b = sum(sum(start_b[i:i+8][j:j+8], []))
+for i in range(N-8+1):
+    for j in range(M-8+1): 
+        w = sum(sum([row[j:j+8] for row in start_w[i:i+8]], []))
+        b = sum(sum([row[j:j+8] for row in start_b[i:i+8]], []))
         result.append(min(w,b))
-        print('i,j:',i,j,i+8, j+8)
-        print('len:',len(start_w[i:i+8][j:j+8]))
-        print(start_w[i:i+8][j:j+8])
 print(min(result)) 
 
 '''
 sum([[1,2],[3,4]]) 로 하면 start의 값이 디폴드로 0이기 때문에, 
 TypeError: unsupported operand type(s) for +: 'int' and 'list' 에러 발생
+(sum 함수가 첫 번째 인자로 0을 사용하고, 두번째 인자부터 주어진 반복 가능한 컬렉션의 각 요소들을 0에 더함)
+
 sum([[1,2],[3,4]], []) 의 형태로 사용해야 [] + [1,2] + [3,4]로 해석되어 
-1차원 리스트를 구할 수 있다
+[1,2,3,4]라는 1차원 리스트를 구할 수 있다
 '''
--- BAEKJOON/CLASS2/18111.py
+++ BAEKJOON/CLASS2/18111.py
@@ -1,112 +1,214 @@
-import sys
-from collections import Counter
+# 2024-04-23
+def calculate(height):
+    inventory = B
+    flag = False
+    time_cnt = 0
+    for i in range(len(ground)):
+        if ground[i] > height: # Remove block (2s)
+            diff = ground[i] - height
+            time_cnt += 2*diff
+            inventory += 1*diff
+        elif ground[i] < height: # Insert block (1s)
+            diff = height - ground[i]
+            time_cnt += 1*diff
+            inventory -= 1*diff
+            if inventory < 0:  # 인벤토리에 블록 부족 -> 높이 재설정 필요
+                flag = True 
+                break
+        else:
+            continue
+    return time_cnt, flag
+
 N, M, B = map(int, input().split())
 ground = []
 for i in range(N):
-    ground.extend(list(map(int, sys.stdin.readline().split())))
+    ground.extend(list(map(int, input().split())))
 
-def cal_time(ground, value, B):
-    time = 0
-    if value>256 or value<0:
-        return 9999, 0
-
-    for i in ground:
-        if i > value: # remove block: 2sec
-            time += 2 *(i-value)
-            B += i-value
-        elif i < value: # put block: 1sec
-            time += 1*(value-i)
-            B -= value-i
-    # 가지고 있는 블록보다 많이 쓴 경우는 B>0이 될 때까지 높은 층부터 한 층 씩 block제거
-    # pre_time = time
-    # while B < 0:
-    #     value -= 1
-    #     remove_block_cnt = ground.count(max(ground))
-    #     B += remove_block_cnt
-    #     time += 2*remove_block_cnt
-    #     ground = [x-1 for x in ground if x==max(ground)] # ground update
-    #     if value<0:
-    #         return 9999, 0
-    #     return time - pre_time, value
-    return time, value
+g_set = set(ground)
+best_time, result_height = 1e8, 0
+for height in range(min(g_set), max(g_set)+1):
+    time_cnt, flag = calculate(height)
+    if not flag and time_cnt <= best_time:
+        best_time = time_cnt
+        result_height = height
+print(best_time, result_height)
 
 
-# mean = cal_time(ground, int(round(sum(ground)/len(ground))), B)
-# mode = cal_time(ground, Counter(ground).most_common()[0][0], B)
-# print(int(round(sum(ground)/len(ground))), Counter(ground).most_common()[0][0])
-# print(mean, mode)
+# from collections import Counter
+# candidate = []
+# # 주어진 height 내에서 빈도수 순서대로 검토
+# counter = Counter(ground)
+# counter = sorted(counter.items(), key=lambda x:(-x[1],-x[0])) # -: 내림차순
 
-res_min = cal_time(ground, min(ground), B)
-for x in range(min(ground)+1, max(ground)+1):
-    res = cal_time(ground, x, B)
-    if res[0]<res_min[0] or (res[0]==res_min[0] and res[1]>res_min[1]):
-        res_min = res
-print(*res)
+# reset = False
+# for k in range(len(counter)):
+#     height = counter[k][0]
+#     time_cnt, flag = calculate(height)
+#     if not flag: # if flag : # 1 more round
+#         break
 
+# candidate.append([time_cnt, height])
+# 평균값 candidate 도 계산 후 best 값 추출
+# result_time, result_height = sorted(candidate)[0]
+# print(result_time, result_height)
 
 '''
-# 경우의 수:
-# 1. 평균값으로 맞추는 경우
-# 2. 최빈값으로 맞추는 경우
-# 3. 인벤토리가 비어있어 put을 못하는 경우
+# 4:38~5:22 (44m) 틀림
 
+문제 조건 중 
+'땅의 높이는 256블록을 초과할 수 없으며, 음수가 될 수 없다'
+이 조건을 고려해주지 않았음
+=> 어차피 주어지는 값 중에서 height를 정하기 때문에 256 보다 크게 설정될 수 없음
+=> 이게 문제였다 
+중간값, 평균값을 height으로 설정할 때 더 적은 시간이 걸릴 수 있음을 간과
 
-반례1)
-1 3 68
-0 0 1
-정답: 2 1
-내답: 2 0
--> 내가 나눈 두 경우의 수 모두 해당 안됨 
--> 저 두가지만 하는게 아니라 min 부터 max까지 다 돌려야 할듯
-그리고 조건 반영 안됨: 답이 여러개 일 시 땅 높이가 높은 것 출력
+# 5:24~5:43 (20m) 
+평균값 고려해줬지만 틀림
 
-반례2)
-3 4 11
-29 51 54 44
-22 44 32 62
-25 38 16 2
-정답: 250 35
-내답: 2 43
+# 5:49~5:57 (8m)
+뭐가 문제일까?
+평균값이 아니라 가능한 모든 height값 (0~256) 돌려보기
+=> 시간초과
+    for문 안에서 함수를 호출하는데 그 함수 내에 for문이 구현되어 있어, 2중 포문 구조이므로 시간초과 
+=> 값의 범위를 좁혀야 하지 않을까/ 최빈값, 평균값 외에 어떤 값이 될 수 있지? 
 
-반례3)
-4 4 36
-15 43 61 21
-19 33 31 55
-48 63 1 30
-31 28 3 8
-정답: 355 32
-내답: 357 31
-
-반례4)
+# 6:08~6:33 (25m)
+candidate 으로 결과값 다 받는 구조를 변수에 best 값 업데이트 하는 방식으로 바꿈
+=> 시간초과
+게시판에서 반례 발견
 2 2 0
-256 256
-0 0
-정답: 768 128
-내답: 4 255
+1 1
+1 5
+[정담] 8 1
+[출력] 2 1
+같은 위치에서 블록을 여러 개 제거해 주는 경우 고려 안함
+=> diff 변수 추가
+가능한 height 범위를 평균값이 아니라 min~max까지 돌려봐야 할듯
+평균값만이 best time을 갖는 height일거라는 근거가 없음
+=> 시간초과
 
-반례4)
-7 7 6000
-30 21 48 55 1 1 4
-0 0 0 0 0 0 0
-15 4 4 4 4 4 8
-20 40 60 10 20 30 2
-1 1 1 1 1 1 9
-24 12 33 7 14 25 3
-3 3 3 3 3 3 32
-정답: 879 10
-내답: 881 12
-
-반례5)
-2 2 35
-20 10
-190 40
-정답: 350 40
-내답: 375 65
-
-반례6)
-2 2 68
-120 90
-250 170
-정답: 290 170
-내답: 314 158
+::: 시간초과 뜨는 이유 분석해보기 :::
 '''
+
+
+
+
+
+
+
+
+
+
+# 2023-01-11
+# import sys
+# from collections import Counter
+# N, M, B = map(int, input().split())
+# ground = []
+# for i in range(N):
+#     ground.extend(list(map(int, sys.stdin.readline().split())))
+
+# def cal_time(ground, value, B):
+#     time = 0
+#     if value>256 or value<0:
+#         return 9999, 0
+
+#     for i in ground:
+#         if i > value: # remove block: 2sec
+#             time += 2 *(i-value)
+#             B += i-value
+#         elif i < value: # put block: 1sec
+#             time += 1*(value-i)
+#             B -= value-i
+#     # 가지고 있는 블록보다 많이 쓴 경우는 B>0이 될 때까지 높은 층부터 한 층 씩 block제거
+#     # pre_time = time
+#     # while B < 0:
+#     #     value -= 1
+#     #     remove_block_cnt = ground.count(max(ground))
+#     #     B += remove_block_cnt
+#     #     time += 2*remove_block_cnt
+#     #     ground = [x-1 for x in ground if x==max(ground)] # ground update
+#     #     if value<0:
+#     #         return 9999, 0
+#     #     return time - pre_time, value
+#     return time, value
+
+
+# # mean = cal_time(ground, int(round(sum(ground)/len(ground))), B)
+# # mode = cal_time(ground, Counter(ground).most_common()[0][0], B)
+# # print(int(round(sum(ground)/len(ground))), Counter(ground).most_common()[0][0])
+# # print(mean, mode)
+
+# res_min = cal_time(ground, min(ground), B)
+# for x in range(min(ground)+1, max(ground)+1):
+#     res = cal_time(ground, x, B)
+#     if res[0]<res_min[0] or (res[0]==res_min[0] and res[1]>res_min[1]):
+#         res_min = res
+# print(*res)
+
+
+# '''
+# # 경우의 수:
+# # 1. 평균값으로 맞추는 경우
+# # 2. 최빈값으로 맞추는 경우
+# # 3. 인벤토리가 비어있어 put을 못하는 경우
+
+
+# 반례1)
+# 1 3 68
+# 0 0 1
+# 정답: 2 1
+# 내답: 2 0
+# -> 내가 나눈 두 경우의 수 모두 해당 안됨 
+# -> 저 두가지만 하는게 아니라 min 부터 max까지 다 돌려야 할듯
+# 그리고 조건 반영 안됨: 답이 여러개 일 시 땅 높이가 높은 것 출력
+
+# 반례2)
+# 3 4 11
+# 29 51 54 44
+# 22 44 32 62
+# 25 38 16 2
+# 정답: 250 35
+# 내답: 2 43
+
+# 반례3)
+# 4 4 36
+# 15 43 61 21
+# 19 33 31 55
+# 48 63 1 30
+# 31 28 3 8
+# 정답: 355 32
+# 내답: 357 31
+
+# 반례4)
+# 2 2 0
+# 256 256
+# 0 0
+# 정답: 768 128
+# 내답: 4 255
+
+# 반례4)
+# 7 7 6000
+# 30 21 48 55 1 1 4
+# 0 0 0 0 0 0 0
+# 15 4 4 4 4 4 8
+# 20 40 60 10 20 30 2
+# 1 1 1 1 1 1 9
+# 24 12 33 7 14 25 3
+# 3 3 3 3 3 3 32
+# 정답: 879 10
+# 내답: 881 12
+
+# 반례5)
+# 2 2 35
+# 20 10
+# 190 40
+# 정답: 350 40
+# 내답: 375 65
+
+# 반례6)
+# 2 2 68
+# 120 90
+# 250 170
+# 정답: 290 170
+# 내답: 314 158
+# '''
