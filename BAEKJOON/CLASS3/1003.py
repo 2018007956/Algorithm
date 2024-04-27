5a8a1c2c022f7a0e1c95e62442889e22a8987a17
2018007956 <bbmt1906@hanyang.ac.kr>
2024-04-27 15:49:43 +0900

    2024-04-27 BAEKJOON 1003 TLE Solved using DP instead of recursion #1

--- BAEKJOON/CLASS3/1003.py
+++ BAEKJOON/CLASS3/1003.py
@@ -1,27 +1,27 @@
-import sys
-input = sys.stdin.readline
-
-cnt0 = 0
-cnt1 = 0
+# Solved (24m)
 def fibonacci(n):
-    global cnt0, cnt1
-    if n==0:
-        cnt0 += 1
-        return 0
-    elif n==1:
-        cnt1 += 1
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+    cache = [(1,0), (0,1)]
+    for i in range(2, n+1):
+        cache.append((sum(cache[i-2]), sum(cache[i-1])))
+    return cache[n]
 
 T = int(input())
 for _ in range(T):
     N = int(input())
-    fibonacci(N)
-    print(cnt0, cnt1)
-    cnt0, cnt1 = 0,0
+    result = fibonacci(N)
+    print(result[0], result[1])
 
 '''
-전역변수로 하나씩 카운팅 -> 시간초과
+1초 -> 10^8
+0.25초 -> 2.5*10^7, 0 <= N <= 40 
 
+<피보나치 수열 구하는 알고리즘>
+1. 재귀함수
+시간복잡도 O(N^2) => 시간초과
+2. DP
+시간복잡도 O(N) => 성공
+3. 행렬 멱법
+점화식을 행렬화 시켜서 푸는 방법
+시간복잡도 O(logN)
+(참고 사이트 : https://myjamong.tistory.com/305)
 '''
