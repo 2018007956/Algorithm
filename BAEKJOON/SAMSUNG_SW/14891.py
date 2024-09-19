17d3ac4a70ca621af9003e03e959a49c292adcdb
2018007956 <chaea11s0@gmail.com>
2024-09-20 01:24:30 +0900

    updated boj 14891 (better code) #13

--- BAEKJOON/SAMSUNG_SW/14891.py
+++ BAEKJOON/SAMSUNG_SW/14891.py
@@ -1,45 +1,38 @@
 # Solved (58m)
-def rotate(num, dir):
-    # 시계 방향 회전
-    if dir == 1: 
-        topni[num] = topni[num][-1] + topni[num][:-1]
-    # 반시계 방향 회전
-    else:
-        topni[num] = topni[num][1:] + topni[num][0]
+from collections import deque
+def right(num, d):
+    if num > 3:
+        return
+    if topni[num-1][2] != topni[num][-2]:
+        right(num+1, -d)
+        topni[num].rotate(d)
+
+def left(num, d):
+    if num < 0:
+        return
+    if topni[num][2] != topni[num+1][-2]:
+        left(num-1, -d)
+        topni[num].rotate(d)
 
 
-topni = [input() for _ in range(4)]
-
+topni = [deque(input()) for _ in range(4)]
 K = int(input())
 for _ in range(K):
-    num, dir = map(int, input().split())
+    num, d = map(int, input().split())
     num -= 1
-    topni_base = topni[:]
-    rotate(num, dir)
 
-    prev_left_isrotate = True
-    prev_right_isrotate = True
-    for i in range(1, 4):
-        if i%2==1:
-            if prev_left_isrotate and 0<=num-i<4 and topni_base[num-i][2] != topni_base[num-i+1][-2]:
-                rotate(num-i, -dir)
-            else:
-                prev_left_isrotate = False
-            if prev_right_isrotate and 0<=num+i<4 and topni_base[num+i][-2] != topni_base[num+i-1][2]:
-                rotate(num+i, -dir)
-            else:
-                prev_right_isrotate = False
-        else:
-            if prev_left_isrotate and 0<=num-i<4 and topni_base[num-i][2] != topni_base[num-i+1][-2]:
-                rotate(num-i, dir)
-            else:
-                prev_left_isrotate = False
-            if prev_right_isrotate and 0<=num+i<4 and topni_base[num+i][-2] != topni_base[num+i-1][2]:
-                rotate(num+i, dir)
-            else:
-                prev_right_isrotate = False
+    left(num-1, -d)
+    right(num+1, -d)
+
+    topni[num].rotate(d)
                 
 score = 0
 for i, x in enumerate(topni):
     score += int(x[0]) * (2**i)
 print(score)
+
+'''
+다른 풀이 공부 https://sooz.tistory.com/106 
+1. deque().rotate() 함수 안에 양수를 넣으면 오른쪽 방향으로, 음수를 넣으면 왼쪽 방향으로 숫자만큼 회전한다
+2. '1번 톱니바퀴는 2번이 회전하지 않았기 때문에, 회전하지 않게 된다.'라는 연쇄 조건은 dfs로 구현
+'''
